543a0d3cec330e9c1f447b73d482f9d399098a99
VIGNESHVICKY6 <vigneshviki814@gmail.com>
2019-11-22 15:51:33 +0530

    first commit

---
+++ source/Home.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="/css/bootstrap.min.css">
+        <script rel="stylesheet" src="/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+        <title></title>
+    
+    </head>
+    <body>
+        <div class="container-fluid  yap">
+         
+            <div class="navbar navbar-inverse ">
+                <div class="navbar-header">
+                    <a class="navbar-brand">AUTO CARE SERVICE CENTER</a>
+                </div>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li class="active"><a href="#">HOME</a></li>
+                        <li><a href="#">SERVICE</a></li>
+                        <li><a href="#">ABOUT</a></li>
+                        <li><a href="#">CONTACT US</a></li>
+                        <li><a href="/login.html" class="glyphicon glyphicon-log-in">&nbsp;LOGIN/SIGNUP</a></li>
+                    </ul>
+            </div></div>
+            <div class="container-fluid">
+                <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                        <li data-target="#myCarousel" data-slide-to="1"></li>
+                        <li data-target="#myCarousel" data-slide-to="2"></li>
+                    </ol>
+                    <div class="carousel-inner">
+                        <div class="item active">
+                            <img src="01.jpg" alt="carl" style= "width: 100%;">
+                        </div>
+                    <div class="item">
+                        <img src="02.jpg" alt="car2" style="width: 100%;">
+                    </div>
+                     <div class="item">
+                        <img src="09.jpg" alt="car3" style="width: 100%;">
+                    </div>
+                </div>
+                <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                 <span class="glyphicon glyphicon-chevron-left"></span>
+                 <span class="sr-only">Previous</span>
+                  </a>
+                <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                  <span class="glyphicon glyphicon-chevron-right"></span>
+                    <span class="sr-only">Next</span>
+                         </a>
+                </div>
+            </div><br><br>
+            
+            <div class="container-fluid">
+                
+                <div class="col-md-3 well " >
+                    <div class=" glyphicon glyphicon-user"  >
+                
+                    AUTO SERVICE
+                        
+                    <p>Welcome Auto Repair Service center</p><br><br>
+                    </div></div>
+                    
+                <div class="col-md-3 well glyphicon glyphicon-search">&nbsp;FREE APPOINTMENT
+                        <br> 1.Free Diagnosis & Brake Checks
+                        <br> 2.Message with photo and video
+                        <br> 3.Expect free call from us<br></div>
+                <div class="col-md-3 well glyphicon glyphicon-time" >&nbsp;WORKING TIME
+                         <br>WEEK DAYS:	 05:00 – 22:00
+                           <br>  SATURDAY:	 08:00 – 18:00
+                           <br>  SUNDAY:	 CLOSED
+                           <br>  EMERGENCY:	 24HR / 7DAYS</div>
+                <div class="col-md-3 well glyphicon glyphicon-map-marker"  >&nbsp;LOCATION<br>
+                    Coimbatore<br><br><br><br></div>
+                
+                     
+                </div>
+                
+                      
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
